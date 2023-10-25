f4901b6f149c4e0b78805c536a72bad99de6ee1b
MuxlisaQuronboyeva10 <147811308+MuxlisaQuronboyeva10@users.noreply.github.com>
2023-10-25 22:24:40 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -10,6 +10,8 @@
 
 int main() {
     std::cout << "Task 1" << std::endl;
+    LeapYear ly;
+    ly.checkLeapYear();
     // call for task 1
     std::cout << "Task 2" << std::endl;
     // call for task 2
