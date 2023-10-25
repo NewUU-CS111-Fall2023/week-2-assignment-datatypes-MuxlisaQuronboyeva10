a6afb6be3593ba59868b4a4c5f8360ce9dcc7ed5
MuxlisaQuronboyeva10 <147811308+MuxlisaQuronboyeva10@users.noreply.github.com>
2023-10-25 22:39:23 +0500

    Update task_4.py

--- src/py/main.py
+++ src/py/main.py
@@ -1,2 +1,13 @@
 # * Author:
 # * Date:
+# Task_4
+def string_matching(sent, word):
+    index = sent.find(word)
+    if index == -1:
+        return "No match"
+    else:
+        return index
+
+sent = input("Enter a sentence: ")
+word = input("Enter a word: ")
+print(string_matching(sent, word))
