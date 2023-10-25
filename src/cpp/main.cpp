315dce788102d0024248e6dbe567507c2ca1b6dd
MuxlisaQuronboyeva10 <147811308+MuxlisaQuronboyeva10@users.noreply.github.com>
2023-10-25 22:40:01 +0500

    Update main.cpp

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -19,11 +19,5 @@ int main() {
     // call for task 2
     std::cout << "Task 3" << std::endl;
     // call for task 3
-    std::cout << "Task 4" << std::endl;
-    Dividing ly;
-    ly.Divider();
-    // call for task 4
-    std::cout << "Task 5" << std::endl;
-    // call for task 5
     return 0;
 }
