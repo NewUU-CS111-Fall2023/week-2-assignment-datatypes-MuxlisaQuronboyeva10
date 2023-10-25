afe33b0365ec9ea1ff4a2a26f417c618fd51eb72
MuxlisaQuronboyeva10 <147811308+MuxlisaQuronboyeva10@users.noreply.github.com>
2023-10-25 22:25:57 +0500

    Update task_1.h

--- src/cpp/task_1.h
+++ src/cpp/task_1.h
@@ -3,3 +3,24 @@
  * Date:
  * Name:
  */
+// leapYear.h
+#ifndef TASK_1_H
+#define TASK_1_H
+
+#include <iostream>
+
+class LeapYear {
+        public:
+        void checkLeapYear() {
+            int year;
+            std::cout << "Enter a year: " << std::endl;
+            std::cin >> year;
+
+            if (year % 4 == 0)
+                std::cout << year << " is a leap year." << std::endl;
+            else
+                std::cout << year << " is not a leap year." << std::endl;
+        }
+};
+
+#endif // TASK_1_H
